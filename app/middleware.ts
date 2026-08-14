b4d8ce380ca380df65703494d244887a415368ed
mainlandwho <victorhperezsales@gmail.com>
2026-08-13 18:44:01 -0700

    Set runtime for middleware to 'nodejs'

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,13 +1,12 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "./lib/supabase/middleware";
 
-export const runtime = "nodejs";
-
 export async function middleware(request: NextRequest) {
   return updateSession(request);
 }
 
 export const config = {
+  runtime: "nodejs",
   matcher: [
     "/((?!_next/static|_next/image|favicon.png|icons|apple-touch-icon.png|manifest.json|sw.js|offline).*)",
   ],
